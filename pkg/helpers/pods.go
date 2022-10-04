a04adca3eabeba84df4c3f2728488b31b557a588
Pablo Chacin <pablochacin@gmail.com>
2022-10-04 11:13:38 +0200

    Use retry in waitPodWaiting
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- pkg/helpers/pods.go
+++ pkg/helpers/pods.go
@@ -1,13 +1,12 @@
 package helpers
 
 import (
-	"errors"
 	"fmt"
 	"time"
 
+	"github.com/grafana/xk6-kubernetes/pkg/utils"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/apimachinery/pkg/api/errors"
 )
 
 // PodHelper defines helper functions for manipulating Pods
@@ -17,63 +16,22 @@ type PodHelper interface {
 	WaitPodRunning(name string, timeout uint) (bool, error)
 }
 
-// podConditionChecker defines a function that checks if a pod satisfies a condition
-type podConditionChecker func(*corev1.Pod) (bool, error)
-
-// waitForCondition watches a Pod in a namespace until a podConditionChecker is satisfied or a timeout expires
-func (h *helpers) waitForCondition(
-	name string,
-	timeout time.Duration,
-	checker podConditionChecker,
-) (bool, error) {
-	options := metav1.ListOptions{
-		FieldSelector: "metadata.name=" + name,
-	}
-	watcher, err := h.client.Structured().Watch(
-		"Pod",
-		h.namespace,
-		options,
-	)
-	if err != nil {
-		return false, err
-	}
-	defer watcher.Stop()
-
-	expired := time.After(timeout)
-	for {
-		select {
-		case <-expired:
-			return false, nil
-		case event := <-watcher.ResultChan():
-			if event.Type == watch.Error {
-				return false, fmt.Errorf("error watching for pod: %v", event.Object)
-			}
-			if event.Type == watch.Modified {
-				pod, isPod := event.Object.(*corev1.Pod)
-				if !isPod {
-					return false, errors.New("received unknown object while watching for pods")
-				}
-				condition, err := checker(pod)
-				if condition || err != nil {
-					return condition, err
-				}
-			}
-		}
-	}
-}
-
 func (h *helpers) WaitPodRunning(name string, timeout uint) (bool, error) {
-	return h.waitForCondition(
-		name,
-		time.Duration(timeout)*time.Second,
-		func(pod *corev1.Pod) (bool, error) {
-			if pod.Status.Phase == corev1.PodFailed {
-				return false, errors.New("pod has failed")
-			}
-			if pod.Status.Phase == corev1.PodRunning {
-				return true, nil
-			}
+	return utils.Retry(time.Duration(timeout)*time.Second, time.Second, func() (bool, error) {
+		pod := &corev1.Pod{}
+		err := h.client.Structured().Get("Pod", name, h.namespace, pod)
+		if errors.IsNotFound(err) {
 			return false, nil
-		},
-	)
+		}
+		if err != nil {
+			return false, err
+		}
+		if pod.Status.Phase == corev1.PodFailed {
+			return false, fmt.Errorf("pod has failed")
+		}
+		if pod.Status.Phase == corev1.PodRunning {
+			return true, nil
+		}
+		return false, nil
+	})
 }
--- pkg/helpers/pods_test.go
+++ pkg/helpers/pods_test.go
@@ -10,23 +10,22 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/watch"
-	k8stest "k8s.io/client-go/testing"
 )
 
 const (
+	podName       = "test-pod"
 	testNamespace = "ns-test"
 )
 
-func buildPod(name, namespace string, status corev1.PodPhase) *corev1.Pod {
-	return &corev1.Pod{
+func buildPod() corev1.Pod {
+	return corev1.Pod{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "v1",
 			Kind:       "Pod",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
+			Name:      podName,
+			Namespace: testNamespace,
 		},
 		Spec: corev1.PodSpec{
 			Containers: []corev1.Container{
@@ -38,7 +37,7 @@ func buildPod(name, namespace string, status corev1.PodPhase) *corev1.Pod {
 			},
 		},
 		Status: corev1.PodStatus{
-			Phase: status,
+			Phase: corev1.PodPending,
 		},
 	}
 }
@@ -47,7 +46,6 @@ func TestPods_Wait(t *testing.T) {
 	t.Parallel()
 	type TestCase struct {
 		test           string
-		name           string
 		status         corev1.PodPhase
 		delay          time.Duration
 		expectError    bool
@@ -58,7 +56,6 @@ func TestPods_Wait(t *testing.T) {
 	testCases := []TestCase{
 		{
 			test:           "wait pod running",
-			name:           "pod-running",
 			delay:          1 * time.Second,
 			status:         corev1.PodRunning,
 			expectError:    false,
@@ -67,7 +64,6 @@ func TestPods_Wait(t *testing.T) {
 		},
 		{
 			test:           "timeout waiting pod running",
-			name:           "pod-running",
 			status:         corev1.PodRunning,
 			delay:          10 * time.Second,
 			expectError:    false,
@@ -76,7 +72,6 @@ func TestPods_Wait(t *testing.T) {
 		},
 		{
 			test:           "wait failed pod",
-			name:           "pod-running",
 			status:         corev1.PodFailed,
 			delay:          1 * time.Second,
 			expectError:    true,
@@ -89,18 +84,27 @@ func TestPods_Wait(t *testing.T) {
 		t.Run(tc.test, func(t *testing.T) {
 			t.Parallel()
 			fake, _ := testutils.NewFakeDynamic()
-			watcher := watch.NewRaceFreeFake()
-			fake.PrependWatchReactor("pods", k8stest.DefaultWatchReactor(watcher, nil))
 			client := resources.NewFromClient(context.TODO(), fake)
 			h := NewHelper(context.TODO(), client, testNamespace)
+			pod := buildPod()
+			_, err := client.Structured().Create(pod)
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+				return
+			}
+
 			go func(tc TestCase) {
-				pod := buildPod(tc.name, testNamespace, tc.status)
+				pod.Status.Phase = tc.status
 				time.Sleep(tc.delay)
-				watcher.Modify(pod)
+				_, e := client.Structured().Update(pod)
+				if !tc.expectError && err != nil {
+					t.Errorf("unexpected error: %v", e)
+					return
+				}
 			}(tc)
 
 			result, err := h.WaitPodRunning(
-				tc.name,
+				podName,
 				tc.timeout,
 			)
 
