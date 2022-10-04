d7c9b59d6cbb685c028589fa5af465356c8eff37
Pablo Chacin <pablochacin@gmail.com>
2022-10-04 13:27:44 +0200

    Document helpers
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

---
+++ examples/wait_pod_running.js
@@ -0,0 +1,40 @@
+import { Kubernetes } from 'k6/x/kubernetes';
+
+let pod = {
+    apiVersion: "v1",
+    kind:       "Pod",
+    metadata: {
+        name:      "busybox",
+        namespace:  "default"
+    },
+    spec: {
+        containers: [
+            {
+                name:    "busybox",
+                image:   "busybox",
+                command: ["sh", "-c", "sleep 30"]
+            }
+        ]
+    }
+}
+
+export default function () {
+  const kubernetes = new Kubernetes();
+
+  // create namespace with random name with prefix 'test-'
+  const ns = kubernetes.helpers().createRandomNamespace("test-")
+  //const ns = "default"
+
+  // create pod in test namespace
+  pod.metadata.namespace = ns
+  kubernetes.create(pod)
+
+  // get helpers for test namespace
+  const helpers = kubernetes.namespacedHelpers(ns)
+
+  // wait for pod to be running
+  const timeout = 10
+  if (!helpers.waitPodRunning(pod.metadata.name, timeout)) {
+      console.log(`"pod ${pod.metadata.name} not ready after ${timeout} seconds`)
+  }
+}
