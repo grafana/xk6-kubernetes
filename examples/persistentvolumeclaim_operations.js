dd0a6619775a8d3190579c9d20f387c8704dbd49
Paul Balogh <javaducky@gmail.com>
2023-04-12 09:32:06 -0500

    Minor script cleanup
    Signed-off-by: Paul Balogh <javaducky@gmail.com>

--- examples/persistentvolumeclaim_operations.js
+++ examples/persistentvolumeclaim_operations.js
@@ -1,6 +1,6 @@
 import { Kubernetes } from 'k6/x/kubernetes';
-import { describe, expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.2/index.js';
-import { load, dump } from 'https://cdn.jsdelivr.net/npm/js-yaml@4.1.0/dist/js-yaml.mjs';
+import { describe, expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.3/index.js';
+import { load } from 'https://cdn.jsdelivr.net/npm/js-yaml@4.1.0/dist/js-yaml.mjs';
 
 let json = {
     apiVersion: "v1",
